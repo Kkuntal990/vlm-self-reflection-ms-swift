78a295a0c2f62b021250a4c3c7776de040f9b69c
Kkuntal990 <kokatekuntal@gmail.com>
2026-01-26 22:00:06 -0800

    Changing to zero2 back

--- scripts/training/run_full_sft_qwen3vl_fire_8gpu.sh
+++ scripts/training/run_full_sft_qwen3vl_fire_8gpu.sh
@@ -168,7 +168,7 @@ swift sft \
     --dataloader_num_workers 4 \
     --dataset_num_proc 4 \
     --save_only_model true \
-    --deepspeed zero3 \
+    --deepspeed zero2 \
     --packing false \
     --dataloader_persistent_workers true \
     --dataloader_prefetch_factor 4 \
